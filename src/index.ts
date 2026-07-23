7929a2b033ff42150baaaabe420f57f23821a45e
zakhar-vasilyev-2005 <vasilyev.zakhar@gmail.com>
2026-07-23 13:21:33 +0300

    1

--- src/index.ts
+++ src/index.ts
@@ -186,6 +186,7 @@ export class Agent extends EventEmitter<AgentEvents> implements AgentParams {
         }
     }
     public async main() {
+
         const agent = this;
         const lines = await this.modelClient.exec("line_list", null);
         await Promise.all(lines.map(e => { this.modelClient.exec("line_free", { line_id: e.line_id }) }));
